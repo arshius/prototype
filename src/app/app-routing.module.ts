8d1d2c2f7c0eae5d197046e902a37d094678cc8f
arshius <arsh.bringer@yahoo.com>
2022-05-18 10:44:59 +0700

    login and home
    add mobile ready login and home page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // {
+  //   path: 'aims', component: HomeComponent, children: [
+  //     // { path: '', component: DashboardComponent },
+  //     // { path: 'user', loadChildren: () => import('./user/user.module').then((user) => user.UserModule) },
+  //   ]
+  // },
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) },
+  { path: '**', redirectTo: '/login', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
